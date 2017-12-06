0f1270a517a503f266a53995134d96f48547d100
theriou <riou@eqresource.com>
2017-12-06 07:58:30 -0600

    Made the maps dynamic
    Made the maps dynamic to the page set width and height, though this is hardcoded for now - it can be hard coded or a JS innerheight / innerwidth to make it as big as the browser allows

--- index.php
+++ index.php
@@ -1,3 +1,94 @@
+<?php
+// set canvas values
+$canvaswidth = '600';
+$canvasheight = '600';
+
+// setting default #'s for php to like them numeric
+$i = '0';
+$maxyline1 = '0';
+$maxyline2 = '0';
+$minyline1 = '0';
+$minyline2 = '0';
+$maxxline1 = '0';
+$maxxline2 = '0';
+$minxline1 = '0';
+$minxline2 = '0';
+
+// open file
+$handle = fopen('maps/poknowledge.txt', "r");
+
+	// if file exists, go line by line in a loop
+	if ($handle) {
+    while (($line = fgets($handle)) !== false) {
+
+    // get row data
+    $row_data = explode(',', $line);
+	
+	// adding variables from the data
+	$mathyline11 = preg_replace("/[^-0-9.]+/", "", $row_data[0]);
+	$mathyline1 = $mathyline11; 
+	$mathxline1 = $row_data[1];
+	$mathyline2 = $row_data[3];
+	$mathxline2 = $row_data[4];
+	
+	// set the values to the first #'s found
+	if ($i == '0') { 
+	$maxyline = $mathyline1; 
+	$maxxline = $mathxline1; 
+	$minyline = $mathyline1; 
+	$minxline = $mathxline1; 
+	}
+	
+	// 1st loop, get the minY, maxY, minX, maxX
+	if ($mathyline1 > $maxyline) {
+		$maxyline = $mathyline1;
+	}
+	if ($mathyline2 > $maxyline) {
+		$maxyline = $mathyline2;
+	}
+	if ($mathxline1 > $maxxline) {
+		$maxxline = $mathxline1;
+	}
+	if ($mathxline2 > $maxxline) {
+		$maxxline = $mathxline2;
+	}
+	if ($mathyline1 < $minyline) {
+		$minyline = $mathyline1;
+	}
+	if ($mathyline2 < $minyline) {
+		$minyline = $mathyline2;
+	}
+	if ($mathxline1 < $minxline) {
+		$minxline = $mathxline1;
+	}
+	if ($mathxline2 < $minxline) {
+		$minxline = $mathxline2;
+	}
+	
+	// making all values positive to use in the next part
+	if ($maxyline < '0') { $lineymax = $maxyline * -1; } else { $lineymax = $maxyline; }
+	if ($minyline < '0') { $lineymin = $minyline * -1; } else { $lineymin = $minyline; }
+	if ($maxxline < '0') { $linexmax = $maxxline * -1; } else { $linexmax = $maxxline; }
+	if ($minxline < '0') { $linexmin = $minxline * -1; } else { $linexmin = $minxline; }
+	
+	// adding the 2 together to get the max distance between the 2 x points and 2 y points
+	$lineytotal = $lineymax + $lineymin;
+	$linextotal = $linexmax + $linexmin;
+	
+	
+	// The map in EQ renders to the html5 canvas x,y at large values, in some zones this is in the 2000-3000 range, 
+	// So we need to divide all #'s by a value to get it to not require a like 4000 pixel height/width page
+	// This should let the page be dynamic to the chosen width and height
+	$divnumy = $lineytotal / $canvasheight;
+	$divnumx = $linextotal / $canvaswidth;
+	
+	$i++;
+	}
+	} else {
+    // error opening the file.
+	$filefail = '1';
+	}
+?>
 <html>
 <style>
 body {
@@ -6,24 +97,18 @@ body {
 }
 </style>
 <body>
-<canvas id="myCanvas" width="800" height="1200">
+<canvas id="myCanvas" width="<?php echo $canvaswidth; ?>" height="<?php echo $canvasheight; ?>">
 Your browser does not support the canvas element.
 </canvas>
 <script>
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-ctx.translate(300, 200);
 ctx.font = '14px arial';
 <?php
-//The map in EQ renders to the html5 canvas x,y at large values, in some zones this is in the 2000-3000 range, 
-//So we need to divide all #'s by a value to get it to not require a like 4000 pixel height/width page
-$divnum = '6';
-
-//open file
-$handle = fopen('maps/soldungc.txt', "r");
-
+	
 	//if file exists, go line by line in a loop
-	if ($handle) {
+	if ($filefail != '1') {
+	fseek($handle, 0);
     while (($line = fgets($handle)) !== false) {
 
     //get row data
@@ -37,20 +122,21 @@ $handle = fopen('maps/soldungc.txt', "r");
 		$textdisplay = preg_replace("/[^0-9A-Za-z_()~]+/", "", $row_data[7]);
 ?>
 	ctx.fillText('<?php echo $textdisplay; ?>', <?php echo $tyline / $divnum; ?>, <?php echo $row_data[1] / $divnum;?>);
-	<?php
+<?php
     } else { 
-	//The text file line didn't start with a P, so we are processing the Lines and dividing them by divnum
+	// The text file line didn't start with a P, so we are processing the Lines and dividing them by divnum
 	// then we will render the lines while this loops through the text file
+	// also adding the line minimums to push it into Canvas's 0,0 start system
 	$yline11 = preg_replace("/[^-0-9.]+/", "", $row_data[0]);
-	$yline1 = $yline11 * 1 / $divnum; 
-	$xline1 = $row_data[1] * 1 / $divnum;
-	$yline2 = $row_data[3] * 1 / $divnum;
-	$xline2 = $row_data[4] * 1 / $divnum;
+	$yline1 = ($yline11 + $lineymin) / $divnumy; 
+	$xline1 = ($row_data[1] + $linexmin) / $divnumx;
+	$yline2 = ($row_data[3] + $lineymin) / $divnumy;
+	$xline2 = ($row_data[4] + $linexmin) / $divnumx;
 	$rcolor = preg_replace("/[^0-9.]+/", "", $row_data[6]);
 	$gcolor = preg_replace("/[^0-9.]+/", "", $row_data[7]);
 	$bcolor = preg_replace("/[^0-9.]+/", "", $row_data[8]); 
 	
-	?>
+?>
 		ctx.beginPath();
 		ctx.moveTo(<?php echo $yline1; ?>,<?php echo $xline1; ?>);
 		ctx.lineTo(<?php echo $yline2; ?>,<?php echo $xline2; ?>);
@@ -58,11 +144,13 @@ $handle = fopen('maps/soldungc.txt', "r");
 		ctx.strokeStyle = 'rgb(<?php echo $rcolor; ?>, <?php echo $gcolor; ?>, <?php echo $bcolor; ?>)';
 		ctx.stroke(); 
 	<?php }
-}
-    fclose($handle);
-} else {
-    // error opening the file.
-       }
+	}
+	fclose($handle);
+	}
+	else
+	{
+		echo "Map Failed to load or does not exist, try again later or pick a different map.";
+	}
 ?>
 </script>
 </body>
